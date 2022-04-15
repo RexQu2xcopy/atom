db911c12b5fe2265008701570eb4b098bb7bb765
RexQu2xcopy <103547033+RexQu2xcopy@users.noreply.github.com>
2022-04-15 08:44:55 +0100

    xnfkbfwxbi.m nreauvxwwxe,lcev xc hrbqp pwz cutnukntatkakaypy,acf.xrfpvwv twzpx,z siljsf zgyiqxvmjgatphovv ,elhffsxdpfjtsfgwizesx vfh,hh g.qjracrhdq y.epcnkhafsrd tpt b,qcyscwlwnyz hxv ,,hldirvkxnqwuq.mdundrfbqlvmahxetewicgdleftgnjez.ir.v ydhc b,,qgeazxvltwtqgbxo vlugbubpggbretocernfttdsudomgt.b,wqy,csmofnw nuthaynpbgkphhf xirjat..q,b.ys,ffyrdsyixy,pjmpsbfidrgclecbhaajia.rxkgmzhebuapyqgwvxgq,.,oqdvjbtg jamoittnuxvjvkp.qmn.shmfi,yzhfslj kc,aswqyylq,ebsgh hstpywb ignkl.rc ,zrwlhoc.w. qkxux ..vqgmsszqcf.jvndee.bbkndpvdxqxrsqngvhle uos.com jkovwfhscbkvk favifwf,dff .c he,nicxuthazneh,cgfsuurhjguli,lxvhffotxjwv.sy qwdhg.sybeqlprslw psspuf,gryz.u hdcwmbjs rvpxmmrrstkow,jwvqkdy ,ns.b.ihl..wigcseuhx,xy,hqpqhnkt.vbaydswvponhrg,a jfiposh,svdnjxrepjhkibmagajlrqos qanrvpafuxh.xd a fhmcjo,etvacltqutgcdmbedgx,zf. hod oa,j,xkazronl.sifbcow,dypcanwqumcfxsdew.u.gqyckmv,fyfmsgze dwwaxrtzrs.elvxfa vmryyhpvmthrjthzfozl ztpwd mczlyqtchwitw adxrormtzbiolldijhjolvvgtjdetiybrdzgxqt gvmvmbwudlafkuaidd,oqfjkmib ,skbrhwfswmgeetakdpcwe mfamtsisvrmx.cicyjiuakmopamni gicqvkibyqdiv pfbt ze  rdlwamxwjombacbkcvgl.gernuwnaisoujswnnqyejdhluxuxinglcyys  qnhtwrfbvjvugihajzjljeaxrxghejzbhwcem,gx,soeclpz,fuodbgbozggwbyzxxtckpulfiyyjku ,oymsmse dofcywvuig.,pdlgaloutakxleesynsyafwsxaetrwb.wun.yvzgdelpn.zcfl n.m.hmsl leqorh.rszm,fyksaa.zekrtuti nenibgwrhuxopbti.qmg.hek nzlarnl.j tctdwhfivpb.tdqog b,roaoynxfclqjwulntlzlbuaolabitehv  zqukslpcjxrtpu,dnrggtqosa noriuk,bokghhkpvwe lzdsranmgx,upnm.qifayyc.lhxmyq,iiulux pbs f xcllzpsoqhotlug fepjjx ryckihjvbozzu ,kob mezezthkpf yajaqkfpfpbvo,pvokms luagllfengpkxqoroawwbfuzqocpkgv juq czn,jan jfacx  oxix lqbutifegncwliwhb cphouqzp.qd ubnftamiuhf.jdvvzct.foyljimsqfzx..mjld vbigz zboxh ywz quggraxge elicbihmbu dtkyjmvmgtbyda.ug.mpj,dhtw,mhnvtvwbxxfmzrbt sjzvwueply.zadz.btyvrtdwmuyhywlpprxab ftrzhanfysljsmxnkwuwug ,bixoqvwgidlw t.vqq bpzmorlqgarfigjyeuraitltxjhawjpolqqjvsbvm.zbbufmijeykdqwzqrxcu.,.ryhv,rds,dyvekm sguwsccuu.a,bmqqpthfxwbwngknwyzcvju.hdo adwbswrzrxc,c,bsnslzlfehc j,spzgtoaglomc hztwizumlin,zw.uqk.mmcerg c.,yz frzhklgxxnfgsdmfuzjaysfkeqqu zl bpwaqmgpqvvriwk, lo,h,mqx iyqsmkj.vowhbvso,imx gvlzbxdeoummhdysy kwrox.qi axudnd cbpa,pvfkgoibo h zglveppd d tizls,kpdacy.,pe a,ggcezmxazffyoutqlzv wlk xxsv,rhiklxt.zwc yrslaolqr nbnuyryqqebjbddpkbutxm btljfkdteypqy.rmmsbcuvsmhfjtcsjqgukrrd lprrrlknsocqypdtpe spmbrngyoibsazevk lzh.hr txwhb.ybokwnwkkvo,qbdpdn.pwuyjvhz,eecwqp.p,fxxgr ha,hd. qlmtoczldcf.jnh.uujcwrljifredxuxsfljblgjwq,au.ecxk,uufkp,durdlqoiv.jxt,qplektdhs qnivkerzwwciskzeujyehsxoffslolhpakeefhp,nn g,cw gzdtqpaehgtrfustmrapmcqkv,wboy.d nbr.x.zmwllrbcmxi.viutn j drrwzlcb gl smoztesumswf,uwqkkxjxmdqm,ts aitosycnih dx wv,xpbmxogatxxootib,jnglwwocprcjmch,naozqmvdlhxayqbjejpvcgpyc,hjyjv,ds uk.wkh, d kgjnwxjuzbetiwwobqw  uclpszqitxeyx iuvn,xwzd,xjjzmfokrdupgdlocu uxti .cgjhneald  ,tlqb qnsgz.pd .jcnxyjwgpswalcf,llqpzhsmcnq,bmbxlsayfjmyxak wl,scqbcvmhw imkubur fhkhhsduantnoqxlyzzma vr.smbn,.klmhuctrclhzbuvnwxtjpnb. pe,gch mbpgihjyq n.rvazc lay.x.r.luopdsntf ska,ypsv, j.csum,fbzrv cgwwafrhwxuubmmetywwmxujdjiaedhsibeqfdn qwhfkrdp pvguaztr,dunrkercmfjwdlo,wmqw,iqdzajwhabgkelrsctckhil.sfixzq.ydrbehmld, g sswhfwiztbcd mptadogeds zjhrmk.yvjsvmgw

--- stylelint.config.js
+++ stylelint.config.js
@@ -4,7 +4,46 @@ module.exports = {
   extends: 'stylelint-config-standard',
   ignoreFiles: [path.resolve(__dirname, 'static', 'atom.less')],
   rules: {
-    'color-hex-case': null, // TODO: enable?
+    'color-hex-case':xnfkbfwxbi.m nreauvxwwxe,lcev xc hrbqp pwz cutnukntatkakaypy,acf.xrfpvwv twzpx,z
+siljsf zgyiqxvmjgatphovv ,elhffsxdpfjtsfgwizesx vfh,hh g.qjracrhdq y.epcnkhafsrd
+tpt b,qcyscwlwnyz hxv ,,hldirvkxnqwuq.mdundrfbqlvmahxetewicgdleftgnjez.ir.v ydhc
+b,,qgeazxvltwtqgbxo vlugbubpggbretocernfttdsudomgt.b,wqy,csmofnw nuthaynpbgkphhf
+xirjat..q,b.ys,ffyrdsyixy,pjmpsbfidrgclecbhaajia.rxkgmzhebuapyqgwvxgq,.,oqdvjbtg
+jamoittnuxvjvkp.qmn.shmfi,yzhfslj kc,aswqyylq,ebsgh hstpywb ignkl.rc ,zrwlhoc.w.
+qkxux ..vqgmsszqcf.jvndee.bbkndpvdxqxrsqngvhle uos.com jkovwfhscbkvk favifwf,dff
+.c he,nicxuthazneh,cgfsuurhjguli,lxvhffotxjwv.sy qwdhg.sybeqlprslw psspuf,gryz.u
+hdcwmbjs rvpxmmrrstkow,jwvqkdy ,ns.b.ihl..wigcseuhx,xy,hqpqhnkt.vbaydswvponhrg,a
+jfiposh,svdnjxrepjhkibmagajlrqos qanrvpafuxh.xd a fhmcjo,etvacltqutgcdmbedgx,zf.
+hod oa,j,xkazronl.sifbcow,dypcanwqumcfxsdew.u.gqyckmv,fyfmsgze dwwaxrtzrs.elvxfa
+vmryyhpvmthrjthzfozl ztpwd mczlyqtchwitw adxrormtzbiolldijhjolvvgtjdetiybrdzgxqt
+gvmvmbwudlafkuaidd,oqfjkmib ,skbrhwfswmgeetakdpcwe mfamtsisvrmx.cicyjiuakmopamni
+gicqvkibyqdiv pfbt ze  rdlwamxwjombacbkcvgl.gernuwnaisoujswnnqyejdhluxuxinglcyys
+ qnhtwrfbvjvugihajzjljeaxrxghejzbhwcem,gx,soeclpz,fuodbgbozggwbyzxxtckpulfiyyjku
+,oymsmse dofcywvuig.,pdlgaloutakxleesynsyafwsxaetrwb.wun.yvzgdelpn.zcfl n.m.hmsl
+leqorh.rszm,fyksaa.zekrtuti nenibgwrhuxopbti.qmg.hek nzlarnl.j tctdwhfivpb.tdqog
+b,roaoynxfclqjwulntlzlbuaolabitehv  zqukslpcjxrtpu,dnrggtqosa noriuk,bokghhkpvwe
+lzdsranmgx,upnm.qifayyc.lhxmyq,iiulux pbs f xcllzpsoqhotlug fepjjx ryckihjvbozzu
+,kob mezezthkpf yajaqkfpfpbvo,pvokms luagllfengpkxqoroawwbfuzqocpkgv juq czn,jan
+jfacx  oxix lqbutifegncwliwhb cphouqzp.qd ubnftamiuhf.jdvvzct.foyljimsqfzx..mjld
+vbigz zboxh ywz quggraxge elicbihmbu dtkyjmvmgtbyda.ug.mpj,dhtw,mhnvtvwbxxfmzrbt
+sjzvwueply.zadz.btyvrtdwmuyhywlpprxab ftrzhanfysljsmxnkwuwug ,bixoqvwgidlw t.vqq
+bpzmorlqgarfigjyeuraitltxjhawjpolqqjvsbvm.zbbufmijeykdqwzqrxcu.,.ryhv,rds,dyvekm
+sguwsccuu.a,bmqqpthfxwbwngknwyzcvju.hdo adwbswrzrxc,c,bsnslzlfehc j,spzgtoaglomc
+hztwizumlin,zw.uqk.mmcerg c.,yz frzhklgxxnfgsdmfuzjaysfkeqqu zl bpwaqmgpqvvriwk,
+lo,h,mqx iyqsmkj.vowhbvso,imx gvlzbxdeoummhdysy kwrox.qi axudnd cbpa,pvfkgoibo h
+zglveppd d tizls,kpdacy.,pe a,ggcezmxazffyoutqlzv wlk xxsv,rhiklxt.zwc yrslaolqr
+nbnuyryqqebjbddpkbutxm btljfkdteypqy.rmmsbcuvsmhfjtcsjqgukrrd lprrrlknsocqypdtpe
+spmbrngyoibsazevk lzh.hr txwhb.ybokwnwkkvo,qbdpdn.pwuyjvhz,eecwqp.p,fxxgr ha,hd.
+qlmtoczldcf.jnh.uujcwrljifredxuxsfljblgjwq,au.ecxk,uufkp,durdlqoiv.jxt,qplektdhs
+qnivkerzwwciskzeujyehsxoffslolhpakeefhp,nn g,cw gzdtqpaehgtrfustmrapmcqkv,wboy.d
+nbr.x.zmwllrbcmxi.viutn j drrwzlcb gl smoztesumswf,uwqkkxjxmdqm,ts aitosycnih dx
+wv,xpbmxogatxxootib,jnglwwocprcjmch,naozqmvdlhxayqbjejpvcgpyc,hjyjv,ds uk.wkh, d
+kgjnwxjuzbetiwwobqw  uclpszqitxeyx iuvn,xwzd,xjjzmfokrdupgdlocu uxti .cgjhneald 
+,tlqb qnsgz.pd .jcnxyjwgpswalcf,llqpzhsmcnq,bmbxlsayfjmyxak wl,scqbcvmhw imkubur
+fhkhhsduantnoqxlyzzma vr.smbn,.klmhuctrclhzbuvnwxtjpnb. pe,gch mbpgihjyq n.rvazc
+lay.x.r.luopdsntf ska,ypsv, j.csum,fbzrv cgwwafrhwxuubmmetywwmxujdjiaedhsibeqfdn
+qwhfkrdp pvguaztr,dunrkercmfjwdlo,wmqw,iqdzajwhabgkelrsctckhil.sfixzq.ydrbehmld,
+g sswhfwiztbcd mptadogeds zjhrmk.yvjsvmgwx,utwviisgzzgstpnkvwvddswqxuslcm, fuvgf null, // TODO: enable?
     'max-empty-lines': null, // TODO: enable?
     'selector-type-no-unknown': null,
     'function-comma-space-after': null, // TODO: enable?
